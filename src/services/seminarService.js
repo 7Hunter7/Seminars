8e79e40d10a7858fbbbaf6daeb173b2f207a6af5
Hunter <i.am.hunter.777.ih@gmail.com>
2025-02-12 18:35:42 +0300

    Updated 'seminarService.js' - Added chacked 'deleteSeminar', 'updateSeminar' and 'createSeminar' with try/catch

--- src/services/seminarService.js
+++ src/services/seminarService.js
@@ -11,46 +11,61 @@ export const getSeminars = async () => {
     return data;
   } catch (error) {
     console.error("Ошибка при получении семинаров:", error);
-    throw error;
+    throw error; // Пробрасываем ошибку дальше, чтобы App.js тоже мог ее обработать
   }
 };
 
 // Удаление семинара
 export const deleteSeminar = async (id) => {
-  const response = await fetch(`${API_URL}/${id}`, {
-    method: "DELETE",
-  });
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
+  try {
+    const response = await fetch(`${API_URL}/${id}`, {
+      method: "DELETE",
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+  } catch (error) {
+    console.error("Ошибка при удалении семинара:", error);
+    throw error;
   }
 };
 
 // Редактирование семинара
 export const updateSeminar = async (id, seminarData) => {
-  const response = await fetch(`${API_URL}/${id}`, {
-    method: "PUT", // или PATCH (в зависимости от требований API)
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(seminarData),
-  });
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
+  try {
+    const response = await fetch(`${API_URL}/${id}`, {
+      method: "PUT", // или PATCH (в зависимости от требований API)
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(seminarData),
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  } catch (error) {
+    console.error("Ошибка при редактировании семинара:", error);
+    throw error;
   }
-  return response.json();
 };
 
 // Создание семинара
 export const createSeminar = async (seminarData) => {
-  const response = await fetch(API_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(seminarData),
-  });
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
+  try {
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(seminarData),
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  } catch (error) {
+    console.error("Ошибка при создании семинара:", error);
+    throw error;
   }
-  return response.json();
 };
