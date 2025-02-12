d5c4be21728119bb49b78a475f805b3c76f225c4
Hunter <i.am.hunter.777.ih@gmail.com>
2025-02-12 11:14:58 +0300

    Updated component 'ConfirmationModal' - Added 'modal-content' with buttons

---
+++ src/components/ConfirmationModal.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ConfirmationModal = ({ isOpen, onClose, onConfirm, message }) => {
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <p>{message}</p>
+        <button onClick={onConfirm}>Подтвердить</button>
+        <button onClick={onClose}>Отмена</button>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmationModal;
