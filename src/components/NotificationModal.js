f2ba1827d2950951c210c4945cd5abf6624d5d9f
Hunter <i.am.hunter.777.ih@gmail.com>
2025-02-12 18:52:20 +0300

    Updated component 'NotificationModal'

--- src/components/NotificationModal.js
+++ src/components/NotificationModal.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import "./Notification.css";
 
 const Notification = ({ message, onClose, type = "success" }) => {
   const [isVisible, setIsVisible] = useState(true);
