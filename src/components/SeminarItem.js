e33c32060576516266e87dbe75f0f49c79804f9b
Hunter <i.am.hunter.777.ih@gmail.com>
2025-02-13 10:50:16 +0300

    Updated component 'SeminarItem' - Added all fields of data

--- src/components/SeminarItem.js
+++ src/components/SeminarItem.js
@@ -2,8 +2,15 @@ import React from "react";
 
 const SeminarItem = ({ seminar, onDelete, onEdit }) => {
   return (
-    <li>
-      {seminar.title} - {seminar.date}
+    <li className="seminar">
+      <div className="seminar__content">
+        <img className="image" src="{seminar.photo}" alt="Seminar photo" />
+        <h2 className="title">{seminar.title}</h2>
+        <p className="description">{seminar.description} </p>
+        <div className="date">
+          Начало в {seminar.time} часов {seminar.date}
+        </div>
+      </div>
       <div className="actions">
         <button onClick={() => onEdit(seminar)}>Редактировать</button>
         <button onClick={() => onDelete(seminar.id)}>Удалить</button>
